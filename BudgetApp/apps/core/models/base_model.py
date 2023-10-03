26a0f031f7426a5f0cbde0d667815a59ee716e94
Sreesanth46 <sreesanth46@gmail.com>
2023-10-03 09:26:31 +0530

    chore: status field

--- BudgetApp/apps/core/models/base_model.py
+++ BudgetApp/apps/core/models/base_model.py
@@ -4,6 +4,7 @@ class BaseModel(models.Model):
     id = models.AutoField(primary_key=True, editable=False, db_column='id')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    status = models.SmallIntegerField(default=1)
     
     class Meta:
         abstract = True
