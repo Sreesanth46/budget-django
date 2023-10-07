98ae3f92669dd43c0ddf95ecc2d0549719ebf500
Sreesanth46 <sreesanth46@gmail.com>
2023-10-08 01:36:06 +0530

    style: spacing

--- BudgetApp/apps/core/models/base_model.py
+++ BudgetApp/apps/core/models/base_model.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class BaseModel(models.Model):
-    id = models.AutoField(primary_key=True, editable=False, db_column='id')
+    id         = models.AutoField(primary_key=True, editable=False, db_column='id')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    status = models.SmallIntegerField(default=1)
+    status     = models.SmallIntegerField(default=1)
     
     class Meta:
         abstract = True
